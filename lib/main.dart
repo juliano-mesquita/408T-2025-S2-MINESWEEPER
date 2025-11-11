0bf38d555c672cc25738de537b62daf84fee1e3a
gabriel <gabrielmbrouwers@gmail.com>
2025-11-11 17:12:06 -0300

    refactor: remoção de titulo

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Menu do Jogo',
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
