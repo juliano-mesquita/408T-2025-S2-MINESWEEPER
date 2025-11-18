587fbeea9fcf39ea139a15fca0098bf058b42081
Pedro Moura <pedrojuts000@gmail.com>
2025-11-18 17:20:53 -0300

    refactor: reoção de linha para limpeza de codigo

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:minesweeper/l10n/app_localizations.dart';
 
 class GamePage extends StatefulWidget {
