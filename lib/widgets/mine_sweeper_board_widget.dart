db8f74dac99637c59c54b87d1e59dab27a054bf6
Pedro Moura <pedrojuts000@gmail.com>
2025-12-09 15:09:09 -0300

    feat: Integração de inicialização de tabuleiro, entre controller e board

--- lib/widgets/mine_sweeper_board_widget.dart
+++ lib/widgets/mine_sweeper_board_widget.dart
@@ -1,59 +1,79 @@
-
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:minesweeper/controller/board_controller.dart';
+import 'package:minesweeper/states/game_state.dart';
+import 'package:minesweeper/states/game_working_state.dart';
+import 'package:watch_it/watch_it.dart';
 
-class MineSweeperBoardWidget extends StatelessWidget
-{
+class MineSweeperBoardWidget extends StatefulWidget with WatchItStatefulWidgetMixin {
   final double cellSize;
 
   const MineSweeperBoardWidget({
     super.key,
     this.cellSize = 70
   });
+  
+  @override
+  State<StatefulWidget> createState() => MineSweeperBoardWidgetState();
+}
+
+class MineSweeperBoardWidgetState extends State<MineSweeperBoardWidget>
+{
+  final controller = GetIt.I.get<BoardController>();
 
   @override
-  Widget build(BuildContext context)
-  {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double bottomPadding = 10;
+  Widget build(BuildContext context) {
+    final gameState = watchIt<GameState>();
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double bottomPadding = 10;
 
-        double availableWidth = constraints.maxWidth;
-        double availableHeight = constraints.maxHeight - bottomPadding;
+          double availableWidth = constraints.maxWidth;
+          double availableHeight = constraints.maxHeight - bottomPadding;
 
-        int columns = (availableWidth / cellSize).floor();
-        int lines = (availableHeight / cellSize).floor();
+          int columns = (availableWidth / widget.cellSize).floor();
+          int lines = (availableHeight / widget.cellSize).floor();
+          int totalCells = lines * columns;
 
-        // TODO: Change to start game
-        // TODO: Do not render board until game is initialized
-        // TODO: Prevent board reinitialization on widget update
-        GetIt.instance.get<BoardController>().initializeBoard(columns, lines);
+          if (gameState.gameWorkingState == GameWorkingState.notStarted) {
+            WidgetsBinding.instance.addPostFrameCallback(
+              (_)
+              {
+                gameState.gameWorkingState = GameWorkingState.playing;
+                controller.initializeBoard(columns, lines);
+              }
+            );
+            // Todo return no build precisa ser widget.
+            // Como não temos uma tela neste momento(ainda não inicializou)
+            // podemos retornar um SizedBox sem tamanho
+            return SizedBox();
+          }
 
-        int totalCells = lines * columns;
+          final board = gameState.board!;
 
-        return Padding(
-          padding: EdgeInsets.only(bottom: bottomPadding),
-          child: GridView.builder(
-            padding: EdgeInsets.zero,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: columns,
-              childAspectRatio: 1.0,
+          return Padding(
+            padding: EdgeInsets.only(bottom: bottomPadding),
+            child: GridView.builder(
+              padding: EdgeInsets.zero,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: columns,
+                childAspectRatio: 1.0,
+              ),
+              itemCount: totalCells,
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue[300],
+                    border: Border.all(color: Colors.white, width: 1),
+                  ),
+                );
+              },
             ),
-            itemCount: totalCells,
-            itemBuilder: (context, index) {
-              return Container(
-                decoration: BoxDecoration(
-                  color: Colors.blue[300],
-                  border: Border.all(color: Colors.white, width: 1),
-                ),
-              );
-            },
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
-  
 }
