acc277052629d9b142788ee12401bda02ed3f264
Pedro Moura <Pedrojuts000@gmail.com>
2025-12-16 13:58:07 -0300

    Feat/ Implementation of symbols on the board (#34)
    * feat: Integração de inicialização de tabuleiro, entre controller e board
    
    * feat: Adicionado para aparecer numeros nas neighborMines, também integração para mostrar os simbolos no tabuleiro (bandeira, bombas, neighborMines)
    
    ---------
    
    Co-authored-by: juliano-mesquita <juliano@mesquita.com.br>

--- lib/widgets/mine_sweeper_board_widget.dart
+++ lib/widgets/mine_sweeper_board_widget.dart
@@ -1,26 +1,23 @@
-
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:minesweeper/controller/board_controller.dart';
 import 'package:minesweeper/states/game_state.dart';
+import 'package:minesweeper/states/game_working_state.dart';
 import 'package:minesweeper/widget/cell_widget.dart';
+import 'package:watch_it/watch_it.dart';
 
-class MineSweeperBoardWidget extends StatefulWidget
-{
+class MineSweeperBoardWidget extends StatefulWidget with WatchItStatefulWidgetMixin {
   final double cellSize;
 
   const MineSweeperBoardWidget({
     super.key,
     this.cellSize = 70
   });
-
-  
   
   @override
-  State<MineSweeperBoardWidget> createState() => _MineSweeperBoardWidgetState();
+  State<StatefulWidget> createState() => MineSweeperBoardWidgetState();
 }
 
-class _MineSweeperBoardWidgetState extends State<MineSweeperBoardWidget>
+class MineSweeperBoardWidgetState extends State<MineSweeperBoardWidget>
 {
   final boardController = GetIt.instance.get<BoardController>();
   final gameState = GetIt.instance.get<GameState>();  
@@ -39,46 +36,66 @@ class _MineSweeperBoardWidgetState extends State<MineSweeperBoardWidget>
   }
 
   @override
-  Widget build(BuildContext context)
-  {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double bottomPadding = 10;
+  Widget build(BuildContext context) {
+    final gameState = watchIt<GameState>();
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double bottomPadding = 10;
 
-        double availableWidth = constraints.maxWidth;
-        double availableHeight = constraints.maxHeight - bottomPadding;
+          double availableWidth = constraints.maxWidth;
+          double availableHeight = constraints.maxHeight - bottomPadding;
 
-        int columns = (availableWidth / widget.cellSize).floor();
-        int lines = (availableHeight / widget.cellSize).floor();
+          int columns = (availableWidth / widget.cellSize).floor();
+          int lines = (availableHeight / widget.cellSize).floor();
+          int totalCells = lines * columns;
 
-        // TODO: Change to start game
-        // TODO: Do not render board until game is initialized
-        // TODO: Prevent board reinitialization on widget update
-        GetIt.instance.get<BoardController>().initializeBoard(columns, lines);
+          if (gameState.gameWorkingState == GameWorkingState.notStarted) {
+            WidgetsBinding.instance.addPostFrameCallback(
+              (_)
+              {
+                gameState.gameWorkingState = GameWorkingState.playing;
+                boardController.initializeBoard(columns, lines);
+              }
+            );
+            // Todo return no build precisa ser widget.
+            // Como não temos uma tela neste momento(ainda não inicializou)
+            // podemos retornar um SizedBox sem tamanho
+            return SizedBox();
+          }
 
-        int totalCells = lines * columns;
+          final board = gameState.board!;
 
-        return Padding(
-          padding: EdgeInsets.only(bottom: bottomPadding),
-          child: GridView.builder(
-            padding: EdgeInsets.zero,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: columns,
-              childAspectRatio: 1.0,
+          return Padding(
+            padding: EdgeInsets.only(bottom: bottomPadding),
+            child: GridView.builder(
+              padding: EdgeInsets.zero,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: columns,
+                childAspectRatio: 1.0,
+              ),
+              itemCount: totalCells,
+              itemBuilder: (context, index) {
+                final x = index % board.width;
+                final y = index ~/ board.width;
+
+                final cell = board.cellAt(x, y);
+
+                return CellWidget(
+                  revealCell: () => revealCell(index),
+                  toggleFlag: () => toggleFlag(index),
+                  cellSize: widget.cellSize,
+                  isRevealed:cell.isRevealed,
+                  isFlagged: cell.isFlagged,
+                  neighborMines: cell.adjacentBombs,
+                  hasMine: cell.isBomb,
+                );
+              },
             ),
-            itemCount: totalCells,
-            itemBuilder: (context, index) {
-              return CellWidget(
-                revealCell: () => revealCell(index),
-                toggleFlag: () => toggleFlag(index),
-                cellSize: widget.cellSize
-              );
-            },
-          ),
-        );
-      },
+          );
+        }
+      )
     );
   }
-  
 }
